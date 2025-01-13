2cc18ec4c94021d5f4e2a64112d6432f15fe7be1
Satyadev Tummala <satyadev9691@gmail.com>
2025-01-12 20:36:58 -0500

    Adding new ServerComponent

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ServerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'Does this change';
+  name = '';
 }
--- my-first-app/src/main.ts
+++ my-first-app/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { ServerComponent } from './app/server/server.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
